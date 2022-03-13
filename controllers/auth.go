7e803860e9b58debf389e046cfe0d6f559bda23a
h4ckit <dammysteve10@gmail.com>
2022-03-13 18:27:44 +0100

    Moved Models And Added More Controller Funcs

---
+++ controllers/auth.go
@@ -0,0 +1,7 @@
+package controllers
+
+import "github.com/gofiber/fiber"
+
+func SignUp(c *fiber.Ctx) error {
+	return nil
+}
---
+++ controllers/unimplemented.go
@@ -0,0 +1,7 @@
+package controllers
+
+import "github.com/gofiber/fiber"
+
+func Unimplemented(c *fiber.Ctx) error {
+	return nil
+}
