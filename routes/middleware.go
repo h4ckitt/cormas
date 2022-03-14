57dca5ed37d7e5e3fd5632014a771ff57fd60902
h4ckit <dammysteve10@gmail.com>
2022-03-14 21:14:47 +0100

    Reorganized Routes

--- routes/middleware.go
+++ routes/middleware.go
@@ -2,9 +2,15 @@ package routes
 
 import (
 	"fmt"
+	jwtware "github.com/gofiber/jwt/v3"
+	"rest-api/config"
 
-	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
 )
+
+var JWTMiddleWare = jwtware.New(jwtware.Config{
+	SigningKey: []byte(config.GetConfig().SecretKey),
+})
 
 func MiddleWare(c *fiber.Ctx) error {
 	fmt.Println("Request")
--- routes/route.go
+++ routes/route.go
@@ -2,20 +2,16 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"rest-api/controllers"
 )
 
-//const secretkey = "Mr.RavandraIsTheB3sTUpw0rKCl13nt"
 func InitRouter() *fiber.App {
 	app := fiber.New()
 
-	/*app.Use(jwtware.New(jwtware.Config{
-		SigningKey: []byte(secretkey),
-	}))*/
-	//x := controllers.SignUpHandler
 	v1 := app.Group("/v1")
 
-	userGroup := v1.Group("/user")
-	userGroup.Get("/", controllers.Unimplemented)
+	setupUserRoutes(v1)
+	setupAnswerRoutes(v1)
+	setupPostRoutes(v1)
+
 	return app
 }
