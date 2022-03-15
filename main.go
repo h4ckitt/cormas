9fcb4b92cd74039226330d2fbda7f9f8219f4b71
h4ckit <dammysteve10@gmail.com>
2022-03-15 08:22:19 +0100

    Moved dgraph Initialization To db Package

--- main.go
+++ main.go
@@ -3,21 +3,7 @@ package main
 import (
 	"log"
 	"rest-api/routes"
-
-	"github.com/dgraph-io/dgo"
-	"github.com/dgraph-io/dgo/protos/api"
-	"google.golang.org/grpc"
 )
-
-func newClient() *dgo.Dgraph {
-	d, err := grpc.Dial("localhost:9080", grpc.WithInsecure())
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return dgo.NewDgraphClient(api.NewDgraphClient(d))
-}
 
 func main() {
 	if err := routes.InitRouter().Listen(":8080"); err != nil {
