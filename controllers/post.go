2659a63cd46dccbf84cc78cafc06e6b4392c01a1
h4ckit <dammysteve10@gmail.com>
2022-03-17 20:53:21 +0100

    update: Added Create Post And Get Posts Is WIP

--- controllers/post.go
+++ controllers/post.go
@@ -1,25 +1,72 @@
 package controllers
 
 import (
-	"fmt"
-	"github.com/dgrijalva/jwt-go"
+	"context"
+	"encoding/json"
+	"github.com/dgraph-io/dgo/v210/protos/api"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"log"
+	"rest-api/models"
 	"rest-api/utils"
 )
 
 func CreatePost(c *fiber.Ctx) error {
-	user := utils.GetJWTUser(c.Locals("user").(*jwt.Token))
+	uid := utils.GetJWTUser(c.Locals("user").(*jwt.Token))
 
-	//	post := new(models.Post)
+	post := new(models.Post)
 
-	//	mu := &api.Mutation{CommitNow: true}
+	if err := c.BodyParser(&post); err != nil {
+		log.Println(err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "An Error Occurred While Processing That Request, Please Try Again Later",
+		})
+	}
 
-	//create user posts
-	fmt.Println(user)
-	return nil
+	if post.Name == "" || post.Description == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid Request	 Body Received",
+		})
+	}
+
+	author := struct {
+		UID string `json:"uid"`
+	}{uid}
+
+	post.Author = author
+	post.Type = "Post"
+
+	postJson, err := json.Marshal(post)
+
+	if err != nil {
+		log.Println(err)
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"message": "An Error Occurred While Processing That Request,, Please Try Again Later",
+		})
+	}
+
+	mutation := &api.Mutation{
+		CommitNow: true,
+		SetJson:   postJson,
+	}
+
+	_, err = dgraph.NewTxn().Mutate(context.Background(), mutation)
+
+	if err != nil {
+		log.Println(err)
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"message": "An Error Occurred While Processing That Request,, Please Try Again Later",
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "Post Created Successfully",
+	})
 }
 
 func ReadPost(c *fiber.Ctx) error {
+	//	uid := c.Params("id")
+
 	return nil
 }
 
