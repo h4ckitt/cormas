88480fadb9a5aee189063403ed986be0b63a05ed
h4ckit <dammysteve10@gmail.com>
2022-03-19 22:06:28 +0100

    fix: Added omitempty To Necessary Fields

--- models/post.go
+++ models/post.go
@@ -4,16 +4,16 @@ type Post struct {
 	Name        string      `json:"name"`
 	Description string      `json:"description"`
 	Author      interface{} `json:"author"`
-	Business    interface{} `json:"business"`
-	Orders      *[]Order    `json:"orders"`
+	Business    interface{} `json:"business,omitempty"`
+	Orders      *[]Order    `json:"orders,omitempty"`
 	Privacy     int         `json:"privacy"`
-	Address     Address     `json:"address"`
+	Address     Address     `json:"address,omitempty"`
 	Moderation  bool        `json:"moderation"`
-	Currency    Currency    `json:"currency"`
+	Currency    *Currency   `json:"currency,omitempty"`
 	Comments    *[]Comment  `json:"comments,omitempty"`
-	Amount      float64     `json:"amount"`
+	Amount      float64     `json:"amount,omitempty"`
 	Reactions   *[]Reaction `json:"reactions,omitempty"`
 	Assets      *[]Asset    `json:"assets,omitempty"`
 	Tags        *[]HashTag  `json:"tags,omitempty"`
-	Type        string      `json:"dgraph.type"`
+	Type        string      `json:"dgraph.type,omitempty"`
 }
