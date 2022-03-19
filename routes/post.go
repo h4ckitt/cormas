b479e324bb782b42ef152751f0a6174b83135be9
h4ckit <dammysteve10@gmail.com>
2022-03-19 22:06:47 +0100

    Updated Post Routes

--- routes/post.go
+++ routes/post.go
@@ -9,4 +9,7 @@ func setupPostRoutes(router fiber.Router) {
 	postGroup := router.Group("/post")
 
 	postGroup.Post("/", authRequired(), controllers.CreatePost)
+	postGroup.Get("/:id", controllers.ReadPost)
+	postGroup.Delete("/:id", authRequired(), controllers.DeletePost)
+	postGroup.Patch("/:id", authRequired(), controllers.UpdatePost)
 }
