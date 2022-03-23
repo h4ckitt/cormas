357d1151ea642b750b16d57b5644500e04346646
h4ckit <dammysteve10@gmail.com>
2022-03-23 21:43:26 +0100

    Completted reaction Resource

--- controllers/reaction.go
+++ controllers/reaction.go
@@ -1,15 +1,19 @@
 package controllers
 
 import (
+	"context"
+	"encoding/json"
+	"github.com/dgraph-io/dgo/v210/protos/api"
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
+	"log"
 	"rest-api/models"
 	"rest-api/utils"
 )
 
 // CreateReaction Discuss reaction model
 func CreateReaction(c *fiber.Ctx) error {
-	uid, err := utils.GetJWTUser(c.Locals("user").(*jwt.Token))
+	_, err := utils.GetJWTUser(c.Locals("user").(*jwt.Token))
 
 	if err != nil {
 		if err.Error() == "invalid JWT Token" {
@@ -22,10 +26,89 @@ func CreateReaction(c *fiber.Ctx) error {
 		})
 	}
 
-	_ := new(models.Reaction)
-	return nil
+	reaction := new(models.Reaction)
+
+	if err := c.BodyParser(reaction); err != nil {
+		log.Println(err)
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Bad request body received",
+		})
+	}
+
+	if reaction.Name == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Bad request body received",
+		})
+	}
+
+	reactionJson, err := json.Marshal(reaction)
+
+	if err != nil {
+		log.Println(err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "An error occurred while processing that request",
+		})
+	}
+
+	_, err = dgraph.NewTxn().Mutate(context.Background(), &api.Mutation{CommitNow: true, SetJson: reactionJson})
+
+	if err != nil {
+		log.Println(err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "An error occurred while processing that request",
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "Reaction created successfully",
+	})
 }
 
 func DeleteReaction(c *fiber.Ctx) error {
+	reactionID := c.Params("id")
 
+	if reactionID == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Bad request body received",
+		})
+	}
+
+	_, err := utils.GetJWTUser(c.Locals("user").(*jwt.Token))
+
+	if err != nil {
+		if err.Error() == "invalid JWT Token" {
+			return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+				"message": "Forbidden",
+			})
+		}
+
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "An error occurred while processing that request",
+		})
+	}
+
+	tbd := struct {
+		UID string `json:"uid"`
+	}{reactionID}
+
+	tbdJson, err := json.Marshal(tbd)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "An error occurred while processing that request",
+		})
+	}
+
+	_, err = dgraph.NewTxn().Mutate(context.Background(), &api.Mutation{CommitNow: true, DeleteJson: tbdJson})
+
+	if err != nil {
+		log.Println(err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "An error occurred while processing that request",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Deleted",
+	})
 }
