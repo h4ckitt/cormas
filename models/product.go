27d023cacf4215c0064eb88c27bcf0e5bcb2ca0f
h4ckit <dammysteve10@gmail.com>
2022-03-23 18:06:25 +0100

    fix: Fixed Up Product Resource

--- models/product.go
+++ models/product.go
@@ -7,21 +7,21 @@ type Product struct {
 	Currency     *Currency `json:"currency"`
 	Category     *Category `json:"category"`
 	//This Should Be A Reverse Relationship To Order Table
-	Reviews               *[]Review `json:"review"`
-	ProductType           int       `json:"type"`
-	Supported             int       `json:"supported"`
-	Downloadable          *[]Asset  `json:"downloadable,omitempty"`
-	Thumbnail             *Asset    `json:"thumbnail,omitempty"`
-	Gallery               *[]Asset  `json:"gallery"`
-	Excerpt               string    `json:"excerpt"`
-	Description           string    `json:"description"`
-	TechnicalInformation  string    `json:"technical_information,omitempty"`
-	AdditionalInformation string    `json:"additional_information,omitempty"`
-	ProductInformation    string    `json:"product_information,omitempty"`
-	ProductGuides         string    `json:"product_guides,omitempty"`
-	Status                int       `json:"status"`
-	Moderation            bool      `json:"moderation"`
-	Address               *Address  `json:"address"`
-	Owner                 *User     `json:"owner"`
-	Type                  string    `json:"dgraph.type,omitempty"`
+	Reviews               *[]Review   `json:"review,omitempty"`
+	ProductType           int         `json:"type"`
+	Supported             int         `json:"supported"`
+	Downloadable          *[]Asset    `json:"downloadable,omitempty"`
+	Thumbnail             *Asset      `json:"thumbnail,omitempty"`
+	Gallery               *[]Asset    `json:"gallery,omitempty"`
+	Excerpt               string      `json:"excerpt,omitempty"`
+	Description           string      `json:"description"`
+	TechnicalInformation  string      `json:"technical_information,omitempty"`
+	AdditionalInformation string      `json:"additional_information,omitempty"`
+	ProductInformation    string      `json:"product_information,omitempty"`
+	ProductGuides         string      `json:"product_guides,omitempty"`
+	Status                int         `json:"status"`
+	Moderation            bool        `json:"moderation"`
+	Address               *Address    `json:"address"`
+	Owner                 interface{} `json:"owner"`
+	Type                  string      `json:"dgraph.type,omitempty"`
 }
