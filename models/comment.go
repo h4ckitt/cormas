9eda72135db074b2060e456e01b3f92917ec5ffd
h4ckit <dammysteve10@gmail.com>
2022-03-26 12:39:44 +0100

    major: Refactored Comments

--- models/comment.go
+++ models/comment.go
@@ -3,15 +3,15 @@ package models
 type Comment struct {
 	UID string `json:"uid,omitempty"`
 	//Title       string      `json:"title"`
-	Description string      `json:"description"`
-	Moderation  bool        `json:"moderation"`
-	Author      interface{} `json:"author"`
-	Address     *Address    `json:"address,omitempty"`
-	Reply       *[]Comment  `json:"reply,omitempty"`
-	Reaction    *[]Reaction `json:"reaction,omitempty"`
-	Post        *Post       `json:"post,omitempty"`
-	CreatedAt   string      `json:"created_at"`
-	UpdatedAt   string      `json:"updated_at"`
-	EntityUID   string      `json:"entity_uid,omitempty"` //Should Be Either Of Pro
-	Type        string      `json:"dgraph.type,omitempty"`
+	Description string       `json:"description"`
+	Moderation  bool         `json:"moderation"`
+	Author      interface{}  `json:"author"`
+	Address     *Address     `json:"address,omitempty"`
+	Reply       *[]Comment   `json:"reply,omitempty"`
+	Reaction    *[]Reaction  `json:"reaction,omitempty"`
+	Post        *interface{} `json:"post,omitempty"`
+	Question    *interface{} `json:"question,omitempty"`
+	CreatedAt   string       `json:"created_at"`
+	UpdatedAt   string       `json:"updated_at"`
+	Type        string       `json:"dgraph.type,omitempty"`
 }
